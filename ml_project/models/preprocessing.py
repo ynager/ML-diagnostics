f31210c93719d0d92168db319766bb3096d5d7d5
Yannik Nager <ynager@student.ethz.ch>
2017-10-09 22:28:45 +0200

    first result submission

--- ml_project/models/preprocessing.py
+++ ml_project/models/preprocessing.py
@@ -6,15 +6,19 @@ from sklearn import preprocessing
 class Normalization(skl.base.BaseEstimator, skl.base.TransformerMixin):
 
     def __init__(self):
-        self.model = preprocessing.StandardScaler()
+        self.scaler = preprocessing.StandardScaler()
+        self.normalizer = preprocessing.Normalizer()
     
     def fit(self, X, y):
-        self.model.fit(X)
+        self.scaler.fit(X)
+        #self.normalizer.fit(X)
         print("Preprocessing fitted:")
         return self
     
     def transform(self, X):
-        X_new = self.model.transform(X)
+        X_new = self.scaler.transform(X)
+        #X_new = self.normalizer.transform(X_new)
+        print("Preprocessing applied")
         
         return X_new
 
