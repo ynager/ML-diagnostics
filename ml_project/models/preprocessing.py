91d15b7c399d65f57c4c7b5eadf959363ea9a7a5
Yannik Nager <ynager@student.ethz.ch>
2017-10-10 18:03:41 +0200

    adding ridge regression and cleanup

--- ml_project/models/preprocessing.py
+++ ml_project/models/preprocessing.py
@@ -8,17 +8,15 @@ class Normalization(skl.base.BaseEstimator, skl.base.TransformerMixin):
     def __init__(self):
         self.scaler = preprocessing.StandardScaler()
         self.normalizer = preprocessing.Normalizer()
-    
+
     def fit(self, X, y):
         self.scaler.fit(X)
         #self.normalizer.fit(X)
         print("Scaler fitted")
         return self
-    
-    def transform(self, X):
-        X_new = self.scaler.transform(X)
-        #X_new = self.normalizer.transform(X_new)
-        print("Scaler applied")
-        
-        return X_new
 
+    def transform(self, X):
+        X_new = self.scaler.fit_transform(X)
+        #X_new = self.normalizer.transform(X)
+        print("Scaler applied")
+        return X_new
