fc2ce25c6f7cb931db37dd31579861bd94456f7f
Yannik Nager <ynager@student.ethz.ch>
2017-10-19 19:53:04 +0200

    adding hist eq to utils

--- ml_project/models/utils.py
+++ ml_project/models/utils.py
@@ -154,3 +154,16 @@ def anisodiff3(stack,niter=1,kappa=50,gamma=0.1,step=(1.,1.,1.),option=1,ploton=
 
     return stackout
 
+def image_histogram_equalization(image, number_bins=256, max_value=255):
+    # from http://www.janeriksolem.net/2009/06/histogram-equalization-with-python-and.html
+    
+    # get image histogram
+    image_histogram, bins = np.histogram(image.flatten(), number_bins, normed=True)
+    cdf = image_histogram.cumsum() # cumulative distribution function
+    cdf = max_value * cdf / cdf[-1] # normalize
+    
+    # use linear interpolation of cdf to find new pixel values
+    image_equalized = np.interp(image.flatten(), bins[:-1], cdf)
+    
+    return image_equalized.reshape(image.shape), cdf
+
