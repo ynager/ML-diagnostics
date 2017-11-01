41492b6b0e0ca500b580bd65a153b0db7dc52080
Yannik Nager <ynager@student.ethz.ch>
2017-11-01 13:36:26 +0100

    adding SVM

--- ml_project/models/classification.py
+++ ml_project/models/classification.py
@@ -19,23 +19,48 @@ class MeanPredictor(BaseEstimator, TransformerMixin):
 from sklearn.ensemble import GradientBoostingClassifier
 class GradientBoostingClassification(BaseEstimator, TransformerMixin):
     
-    def __init__(self,learning_rate=0.1, n_estimators=100, verbose=1, subsample=1, max_depth=3):
+    def __init__(self,learning_rate=0.1, n_estimators=100, verbose=1, subsample=1, max_depth=3, sw = True):
         self.learning_rate = learning_rate
         self.n_estimators = n_estimators
         self.verbose = verbose
         self.subsample = subsample
         self.max_depth = max_depth
+        self.sw = sw
                  
                  
-    def fit(self, X, y):
+    def fit(self, X, y, sample_weight=None):
+        
         self.model = GradientBoostingClassifier(learning_rate=self.learning_rate,
                                                 n_estimators=self.n_estimators,
                                                 subsample=self.subsample,
                                                 verbose=self.verbose,
                                                 max_depth=self.max_depth)
-                                                
-        y = np.argmax(y,axis=1)
-        self.model.fit(X, y)
+        
+        if self.sw:
+            Xnew = np.zeros((X.shape[0]*y.shape[1],X.shape[1]))
+            ynew = np.zeros((X.shape[0]*y.shape[1]))
+            w = np.zeros((X.shape[0]*y.shape[1]))
+            
+            for i in range(X.shape[0]):
+                Xnew[y.shape[1]*i:y.shape[1]*i+y.shape[1],:] = X[i,:]
+                ynew[y.shape[1]*i:y.shape[1]*i+y.shape[1]] = np.arange(y.shape[1])
+                w[y.shape[1]*i:y.shape[1]*i+y.shape[1]] = y[i,:]
+        
+            print(X[0,0:50])
+            print(Xnew[2,0:50])
+            print(Xnew[3,0:50])
+            print(y[0])
+            print(ynew)
+            print(w)
+            self.model.fit(Xnew, ynew, w)
+
+    
+        else:
+            y = np.argmax(y,axis=1)
+            self.model.fit(X, y)
+        
+        return self
+
 
     def predict(self, X):
         self.model.predict(X)
@@ -47,6 +72,50 @@ class GradientBoostingClassification(BaseEstimator, TransformerMixin):
     def score(self, X, y):
         ypred = self.predict_proba(X)
         return np.mean(stats.spearmanr(ypred,y,axis=1).correlation)
+
+from sklearn.svm import SVC
+class SupportVectorClassification(BaseEstimator, TransformerMixin):
+
+    def __init__(self, C=1, kernel='rbf', probability=True):
+        self.probability = probability
+        self.kernel = kernel
+        self.C = C
+
+    def fit(self, X, y):
+        self.model = SVC(C=self.C, kernel=self.kernel, probability=self.probability, verbose=True)
+    
+    
+        if True:
+            Xnew = np.zeros((X.shape[0]*y.shape[1],X.shape[1]))
+            ynew = np.zeros((X.shape[0]*y.shape[1]))
+            w = np.zeros((X.shape[0]*y.shape[1]))
+            
+            for i in range(X.shape[0]):
+                Xnew[y.shape[1]*i:y.shape[1]*i+y.shape[1],:] = X[i,:]
+                ynew[y.shape[1]*i:y.shape[1]*i+y.shape[1]] = np.arange(y.shape[1])
+                w[y.shape[1]*i:y.shape[1]*i+y.shape[1]] = y[i,:]
+    
+            self.model.fit(Xnew, ynew, w)
+        
+        
+        else:
+            y = np.argmax(y,axis=1)
+            self.model.fit(X, y)
+
+        return self
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
+
+    def score(self, X, y):
+        ypred = self.predict_proba(X)
+        return np.mean(stats.spearmanr(ypred,y,axis=1).correlation)
+
+
+
 
 
 
--- ml_project/models/feature_extraction.py
+++ ml_project/models/feature_extraction.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
 
 class Crop(BaseEstimator, TransformerMixin):
     def __init__(self, xmin, xmax, ymin, ymax, zmin, zmax):
@@ -16,4 +17,37 @@ class Crop(BaseEstimator, TransformerMixin):
         X = X.reshape(-1, 176, 208, 176)
         X_new = X[:, self.xmin:self.xmax, self.ymin:self.ymax, self.zmin:self.zmax]
         X_new = X_new.reshape(X_new.shape[0], -1)
+
         return X_new
+
+class Histogramize3d(BaseEstimator, TransformerMixin):
+    def __init__(self, n_bins, rmin, rmax):
+        self.rmin = rmin
+        self.rmax = rmax
+        self.n_bins = n_bins
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+
+        if (X.ndim == 5):
+            H = np.zeros((X.shape[0], X.shape[1], self.n_bins), dtype=np.int32)
+            for samp in range(X.shape[0]):
+                for cub in range(X.shape[1]):
+                    H[samp, cub, :] = np.histogram(X[samp,cub, :],
+                                                   bins=self.n_bins,
+                                                   range=(self.rmin,
+                                                          self.rmax))[0]
+        else:
+            H = np.zeros((X.shape[0], self.n_bins), dtype=np.int32)
+            for samp in range(X.shape[0]):
+                H[samp, :] = np.histogram(X[samp, :],
+                                          bins=self.n_bins,
+                                          range=(self.rmin,
+                                                 self.rmax))[0]
+
+
+        Hflat = H.reshape(H.shape[0], -1)
+        return Hflat
+        
