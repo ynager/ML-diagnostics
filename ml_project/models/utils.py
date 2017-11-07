072639b336b2479a378535cad4dc8502d331f094
Yannik Nager <ynager@student.ethz.ch>
2017-11-07 21:12:35 +0100

    some changes

--- ml_project/models/utils.py
+++ ml_project/models/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from sklearn.model_selection import cross_val_score
 from sklearn.cross_validation import StratifiedKFold
+from sklearn.model_selection import KFold
 from sklearn.externals import joblib
 from sklearn.utils import resample
 
@@ -13,9 +14,8 @@ def crossvalscore(path_m, path_x, path_y, k):
     y_prob = np.loadtxt(path_y)
     
     yn = np.argmax(y_prob,axis=1)
-    
-    #print(yn)
     cv = StratifiedKFold(yn, k)
+    #cv = KFold(n_splits=k)
     print("Starting " + str(k) + "-fold cross-validation...")
     scores = cross_val_score(clf, X, y_prob, cv=cv)
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
